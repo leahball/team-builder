7b1b0d0e6c72c6193e0385e622fdbbc94e6710d8
iamleahb <leah.ball@gmail.com>
2021-12-14 23:33:59 -0600

    followed homework to here

--- team-builder/src/Components/App.js
+++ team-builder/src/Components/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Form from './Form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const initialFormValues = {
+  username: "",
+  email: "",
+  role: "",
+};
+
+
+
+export default function App() {
+  const [members, setMembers] = useState([]);
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const updateForm = (inputName, inputValue) => {
+    const newFormValues = { ...formValues, [inputName]: inputValue };
+    setFormValues(newFormValues);
+  };
+
+  const submitForm = () => {
+    const newMember = {
+      username: formValues.username.trim(),
+      email: formValues.email.trim(),
+      role: formValues.role,
+    };
+
+    axios
+      .post("fakeapi.com", newMember)
+      .then((res) => {
+        const newMembers = [...members, newMember];
+        setMembers(newMembers);
+      })
+      .catch((err) => console.log(err));
+    //  d) also on success clear the form
+    setFormValues(initialFormValues);
+  };
+
+  useEffect(() => {
+    axios.get("fakeapi.com").then((res) => setMembers(res.data));
+  }, []);
+
+    return (
+      <div className="container">
+        <h1>Team Member App</h1>
+        <Form formValues={formValues} update={updateForm} submit={submitForm} />
+      </div>
+    );
+  };
 }
-
-export default App;
